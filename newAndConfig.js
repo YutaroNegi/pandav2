ab543d533c13000c0aed433477abac5579fa0ce0
Yutaro Negi <yutaro.sousa@liveuniversity.com>
2022-01-19 14:29:13 -0300

    update

---
+++ newAndConfig.js
@@ -0,0 +1,2 @@
+(()=>{var e;Office.onReady((function(i){(e=Office.context.mailbox.item).sender.emailAddress,e.sender.displayName,toEmail=e.to[0].emailAddress}))})();
+//# sourceMappingURL=newAndConfig.js.map
